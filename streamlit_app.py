91295532e56deffb510e8de76507c3f402fdf579
rnamboze <154571664+rnamboze@users.noreply.github.com>
2024-10-20 14:34:03 -0400

    Code for API Key

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,11 @@ import streamlit as st
 from openai import OpenAI
 import os
 
+### Load your API Key
+my_secret_key = st.secrets['MyOpenAIKey']
+os.environ["OPENAI_API_KEY"] = my_secret_key
+
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
