56555f5dc49528dde560fe721cd4dd33a5b52a4f
rnamboze <154571664+rnamboze@users.noreply.github.com>
2024-10-21 12:34:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ model = AutoModelForCausalLM.from_pretrained("gpt2")
 st.title("😎Explore My GPT2 Helper🤓")
 
 # Instructions
-st.subheader("Enter _what NOUN is on your mind_ followed by an auxilliary verb such as _is, was, has,_ etc. and see what happens! :sunglasses:", divider=True)
+st.subheader("Enter _a NOUN that is on your mind_ followed by an auxilliary verb such as _is, was, has,_ etc. and see what happens! :sunglasses:", divider=True)
 st.caption("The example _Coffee is_ has been provided below")
 
 # Field for the user's prompt
